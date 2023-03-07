b5dab10a0a409b7daf9bdf6f12d8091a55d82356
Corentin <corentin.even.ce@gmail.com>
2023-03-07 10:39:54 +0100

    refacto du raycasting

--- include/display.h
+++ include/display.h
@@ -10,10 +10,20 @@ void displayMinimap(SDL_Renderer *renderer, struct Settings settings);
 
 void displayPlayerOnMinimap(SDL_Renderer *renderer, struct Player player);
 
-void displayScreen(SDL_Renderer *renderer, struct Settings settings, struct Player player, struct Compass compass, int display);
+void displayScreen(SDL_Renderer *renderer, struct Settings settings, struct Player player, struct Compass compass,
+                   int display);
 
 void displayCompass(SDL_Renderer *renderer, struct Player player, struct Compass compass);
 
+void fillScreen(SDL_Renderer *renderer, SDL_Color color);
+
 void drawCircle(SDL_Renderer *renderer, SDL_Color color, int x, int y, int radius);
+
+double dda(struct DVector deltaDist, struct DVector rayDir, struct Player player,
+           struct Settings settings);
+
+struct IVector getStep(struct DVector rayDir);
+
+struct DVector geDist(struct Player player, struct DVector rayDir, struct DVector deltaDist);
 
 #endif //CUB_3D_DISPLAY_H
--- sources/display.c
+++ sources/display.c
@@ -6,11 +6,7 @@
 
 void displayScreen(SDL_Renderer *renderer, struct Settings settings, struct Player player, struct Compass compass,
                    int display) {
-    SDL_SetRenderDrawColor(renderer, settings.c0.r, settings.c0.g, settings.c0.b, 255);
-    SDL_Rect rect = {0, 0, settings.width, settings.height};
-    if (SDL_RenderFillRects(renderer, &rect, 1) != 0) {
-        printf("Error: %s\n", SDL_GetError());
-    }
+    fillScreen(renderer, settings.c0);
     displayMinimap(renderer, settings);
     displayPlayerOnMinimap(renderer, player);
 
@@ -19,60 +15,11 @@ void displayScreen(SDL_Renderer *renderer, struct Settings settings, struct Play
             double cameraX = 2 * x / (double) settings.width - 1;
             struct DVector rayDir = {player.dir.x + player.plane.x * cameraX, player.dir.y + player.plane.y * cameraX};
 
-            //which box of the map we're in
-            struct IVector map = {(int) player.pos.x, (int) player.pos.y};
-
-            //length of ray from current position to next x or y-side
-            struct DVector sideDist;
-
             //length of ray from one x or y-side to next x or y-side
             struct DVector deltaDist = {(rayDir.x == 0) ? 1e30 : fabs(1 / rayDir.x),
                                         (rayDir.y == 0) ? 1e30 : fabs(1 / rayDir.y)};
             // If rayDirX or rayDirY are 0, the division through zero is avoided by setting it to a very high value 1e30
-            double perpWallDist;
-
-            //what direction to step in x or y-direction (either +1 or -1)
-            struct IVector step;
-
-            //calculate step and initial sideDist
-            if (rayDir.x < 0) {
-                step.x = -1;
-                sideDist.x = (player.pos.x - map.x) * deltaDist.x;
-            } else {
-                step.x = 1;
-                sideDist.x = (map.x + 1.0 - player.pos.x) * deltaDist.x;
-            }
-            if (rayDir.y < 0) {
-                step.y = -1;
-                sideDist.y = (player.pos.y - map.y) * deltaDist.y;
-            } else {
-                step.y = 1;
-                sideDist.y = (map.y + 1.0 - player.pos.y) * deltaDist.y;
-            }
-
-
-            int hit = 0; //was there a wall hit?
-            int side; //was a NS or an EW wall hit?
-
-            //perform DDA
-            while (hit == 0) {
-                //jump to next map square, either in x-direction, or in y-direction
-                if (sideDist.x < sideDist.y) {
-                    sideDist.x += deltaDist.x;
-                    map.x += step.x;
-                    side = 0;
-                } else {
-                    sideDist.y += deltaDist.y;
-                    map.y += step.y;
-                    side = 1;
-                }
-                //Check if ray has hit a wall
-                if (settings.map[map.y][map.x] != '0') hit = 1;
-            }
-
-            //Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
-            if (side == 0) perpWallDist = (sideDist.x - deltaDist.x);
-            else perpWallDist = (sideDist.y - deltaDist.y);
+            double perpWallDist = dda(deltaDist, rayDir, player, settings);
 
             //Calculate height of line to draw on screen
             int lineHeight = (int) (settings.height / perpWallDist);
@@ -83,15 +30,98 @@ void displayScreen(SDL_Renderer *renderer, struct Settings settings, struct Play
             int drawEnd = lineHeight / 2 + settings.height / 2;
             if (drawEnd >= settings.height)drawEnd = settings.height - 1;
 
-            //draw the pixels of the stripe as a vertical line;
+            //draw the pixels of the stripe as a vertical line
+            SDL_SetRenderDrawColor(renderer, settings.c2.r, settings.c2.g, settings.c2.b, 255);
+            SDL_RenderDrawLine(renderer, settings.width - x - 1, 0, settings.width - x - 1, drawStart);
             SDL_SetRenderDrawColor(renderer, settings.c1.r, settings.c1.g, settings.c1.b, 255);
             SDL_RenderDrawLine(renderer, settings.width - x - 1, drawStart, settings.width - x - 1, drawEnd);
+            SDL_SetRenderDrawColor(renderer, settings.c0.r, settings.c0.g, settings.c0.b, 255);
+            SDL_RenderDrawLine(renderer, settings.width - x - 1, drawEnd, settings.width - x - 1, settings.height);
         }
     }
 
     displayMinimap(renderer, settings);
     displayCompass(renderer, player, compass);
     displayPlayerOnMinimap(renderer, player);
+}
+
+struct IVector getStep(struct DVector rayDir) {
+    //what direction to step in x or y-direction (either +1 or -1)
+    struct IVector step;
+
+    if (rayDir.x < 0) {
+        step.x = -1;
+    } else {
+        step.x = 1;
+    }
+    if (rayDir.y < 0) {
+        step.y = -1;
+    } else {
+        step.y = 1;
+    }
+
+    return step;
+}
+
+struct DVector geDist(struct Player player, struct DVector rayDir, struct DVector deltaDist) {
+    struct DVector sideDist;
+
+    //which box of the map we're in
+    struct IVector map = {(int) player.pos.x, (int) player.pos.y};
+
+    //calculate step and initial sideDist
+    if (rayDir.x < 0) {
+        sideDist.x = (player.pos.x - map.x) * deltaDist.x;
+    } else {
+        sideDist.x = (map.x + 1.0 - player.pos.x) * deltaDist.x;
+    }
+    if (rayDir.y < 0) {
+        sideDist.y = (player.pos.y - map.y) * deltaDist.y;
+    } else {
+        sideDist.y = (map.y + 1.0 - player.pos.y) * deltaDist.y;
+    }
+
+    return sideDist;
+}
+
+double dda(struct DVector deltaDist, struct DVector rayDir, struct Player player,
+           struct Settings settings) {
+
+
+    //which box of the map we're in
+    struct IVector map = {(int) player.pos.x, (int) player.pos.y};
+
+    //length of ray from current position to next x or y-side
+    struct DVector sideDist = geDist(player, rayDir, deltaDist);
+
+    //what direction to step in x or y-direction (either +1 or -1)
+    struct IVector step = getStep(rayDir);
+
+    int hit = 0; //was there a wall hit?
+    int side; //was a NS or an EW wall hit?
+
+    //perform DDA
+    while (hit == 0) {
+        //jump to next map square, either in x-direction, or in y-direction
+        if (sideDist.x < sideDist.y) {
+            sideDist.x += deltaDist.x;
+            map.x += step.x;
+            side = 0;
+        } else {
+            sideDist.y += deltaDist.y;
+            map.y += step.y;
+            side = 1;
+        }
+        //Check if ray has hit a wall
+        if (settings.map[map.y][map.x] != '0') hit = 1;
+    }
+
+    //Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
+    double perpWallDist;
+    if (side == 0) perpWallDist = (sideDist.x - deltaDist.x);
+    else perpWallDist = (sideDist.y - deltaDist.y);
+
+    return perpWallDist;
 }
 
 void displayMinimap(SDL_Renderer *renderer, struct Settings settings) {
@@ -142,6 +172,11 @@ void displayPlayerOnMinimap(SDL_Renderer *renderer, struct Player player) {
     int x2 = x1 + (int) (30 * (player.dir.x));
     int y2 = y1 + (int) (30 * player.dir.y);
     SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+}
+
+void fillScreen(SDL_Renderer *renderer, SDL_Color color) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderClear(renderer);
 }
 
 void drawCircle(SDL_Renderer *renderer, SDL_Color color, int x, int y, int radius) {
